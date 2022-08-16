d6d507be275d48dae3d44632d338afc48cb4eea1
Erick Ogaro <ogaroerick1998@gmail.com>
2022-08-16 16:23:09 +0300

    unit tests

--- course-02/exercises/udacity-c2-basic-server/src/unit-test-examples/units.ts
+++ course-02/exercises/udacity-c2-basic-server/src/unit-test-examples/units.ts
@@ -13,10 +13,6 @@ export const divide = (a: number, b: number) => {
 };
 
 // try creating a method "concat" to concatenate two strings
-// it should take two string paramaters.
-// it should return one string combining the two strings.
-// it should throw an error if either of the strings are empty.
-// ensure your function is exported.
 export const concat = (a: string, b: string) => {
   if (a.length === 0 || b.length === 0) {
     throw new Error("empty string");
