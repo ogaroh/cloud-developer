6aa923b47c87aa4c1d3ce698ebe3b34021e2c226
Erick Ogaro <ogaroerick1998@gmail.com>
2022-08-23 12:26:26 +0300

    eb deployment updates

--- course-02/exercises/udacity-c2-restapi/src/config/config.ts
+++ course-02/exercises/udacity-c2-restapi/src/config/config.ts
@@ -1,22 +1,27 @@
 export const config = {
-  "dev": {
-    "username": "",
-    "password": "",
-    "database": "",
-    "host": "",
-    "dialect": "postgres",
-    "aws_region": "us-east-2",
-    "aws_profile": "default",
-    "aws_media_bucket": "udagram-ruttner-dev"
+  dev: {
+    username: process.env.POSTGRES_USERNAME,
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.POSTGRES_DATABASE,
+    host: process.env.POSTGRES_HOST,
+    port: 5432,
+    aws_region: process.env.AWS_REGION,
+    aws_profile: "DEPLOYED",
+    aws_media_bucket: process.env.AWS_MEDIA_BUCKET,
+    dialect: "postgres",
   },
-  "jwt": {
-    "secret": " "
+  jwt: {
+    secret: "",
   },
-  "prod": {
-    "username": "",
-    "password": "",
-    "database": "udagram_prod",
-    "host": "",
-    "dialect": "postgres"
-  }
-}
+  prod: {
+    username: "",
+    password: "",
+    database: "",
+    host: "",
+    port: 5432,
+    dialect: "postgres",
+    aws_region: "",
+    aws_profile: "default",
+    aws_media_bucket: "",
+  },
+};
